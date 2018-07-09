97d664b8195585b9fc4c6192a51f4f2466653473
Kat Lasota <lasota.kat@gmail.com>
2018-07-09 17:19:22 +0200

    add working modal part 1

--- js/script.js
+++ js/script.js
@@ -1,88 +1,140 @@
+'use strict';
+//modale
+(function(){ 
+  var modals = document.querySelectorAll('.modal');
+//open modal
+  
+var showModal = function(event){
+  var hashIndex = event.target.href.indexOf('#')
+  var modalId = event.target.href.substring(hashIndex)
+  event.preventDefault();
+  document.querySelector('#modal-overlay').classList.add('show');
+  document.querySelector(modalId).classList.add('show');
+  var endResult = document.querySelector('#modal-one');
+  endResult.innerHTML = '<header>GAME OVER</header>  <div class="content"> <p>Finall result: You:'+params.wins+ ' - Computer:'+params.loses+'</p> </div>'
+};
+ 
+  
+var modalLinks = document.querySelectorAll('.show-modal');
+
+for(var i = 0; i < modalLinks.length; i++){
+  modalLinks[i].addEventListener('click', showModal);
+  }
+
+// hide modal	
+var hideModal = function(event){
+  event.preventDefault();
+  document.querySelector('#modal-overlay').classList.remove('show');
+  var modale = document.querySelectorAll('.modal')
+  for(var i = 0; i < modale.length; i++){
+    modale[i].classList.remove('show');
+  }
+};
+
+var closeButtons = document.querySelectorAll('.modal .close');
+
+for(var i = 0; i < closeButtons.length; i++){
+  closeButtons[i].addEventListener('click', hideModal);
+}
+
+document.querySelector('#modal-overlay').addEventListener('click', hideModal);
+     
+
+for(var i = 0; i < modals.length; i++){
+  modals[i].addEventListener('click', function(event){
+    event.stopPropagation();
+  });
+}
+
+})(); 
 
 var output = document.getElementById("output"); 
 var comment = document.getElementById("comment"); 
 var result = document.getElementById("result"); 
 var round = document.getElementById("round"); 
-var rounds = 0;
+var params = {
+rounds: 0,
+loses: 0,
+wins: 0,
+games: 0
+};
+
 var button = document.getElementById('start-game');
 button.addEventListener('click', newGame);
 function newGame(){
-  rounds = window.prompt('How many rounds you would like to play');
-  if (!isNaN(rounds) && rounds>0){
+  params.rounds = window.prompt('How many params.rounds you would like to play');
+  if (!isNaN(params.rounds) && params.rounds>0){
    document.getElementById("game").style.display = "block";
-   round.innerHTML = '<br> You have ' +rounds+' rounds.'
+   round.innerHTML = '<br> You have ' +params.rounds+' rounds.'
   } else {
     output.innerHTML = 'Input needs to be a number' + '<br><br>' 
   }
 }
 
-var rock = document.getElementById('rock');
-var paper = document.getElementById('paper');
-var scissors = document.getElementById('scissors');
-
-rock.addEventListener('click', user);
-paper.addEventListener('click', user);
-scissors.addEventListener('click', user);
-var loses = 0;
-var wins = 0;
-var games = 0;  
+var moves = document.querySelectorAll('.player-move');
+for (var i=0; i < moves.length; i++) {
+  moves[i].addEventListener('click', user) 
+};
+ 
 
 function results() {
-  if (games < rounds) {  
-  result.innerHTML = 'You:'+wins+ ' - Computer:'+loses+'<br><br> Games played: '+games+'.'
+  if (params.games < params.rounds) {  
+  result.innerHTML = 'You:'+params.wins+ ' - Computer:'+params.loses+'<br><br> games played: '+params.games+'.'
   }
 }
 
 function user(){
-  var playerMove = this.id;
-  
+  var playerMove = this.getAttribute('data-move');
+
   var compMove = Math.floor(Math.random() * 3);
     if (compMove == 0)  {compMove = "rock";
     } else if(compMove == 1) {compMove = "paper";
     } else{compMove = "scissors";
-   }; 
-   function compare (player, comp) {
-     if (player === comp){
-       comment.innerHTML = 'It`s a tie!';
-     } else if (player == 'rock'){
-        if (comp == 'scissors') {
-         wins++;
-         comment.innerHTML = 'You won!';
-        } else if (comp == 'paper')  {
-          loses++;
-          comment.innerHTML = 'You lost!';
-        } 
-      } else if (player == 'paper'){
-        if (comp == 'rock') {
-          wins++;
-         comment.innerHTML = 'You won!';
-        } else if (comp == 'scissors')  {
-          loses++;
-          comment.innerHTML = 'You lost!';
-        } 
-      } else if (player == 'scissors'){
-        if (comp == 'paper') {
-          wins++;
+    }; 
+    function compare (player, comp) {
+      if (player === comp){
+        comment.innerHTML = 'It`s a tie!';
+      } else if (player == 'rock'){
+          if (comp == 'scissors') {
+          params.wins++;
           comment.innerHTML = 'You won!';
-        } else if (comp == 'rock')  {
-          loses++;
-          comment.innerHTML = 'You lost!';
-        } 
-   } 
-}
+          } else if (comp == 'paper')  {
+            params.loses++;
+            comment.innerHTML = 'You lost!';
+          } 
+        } else if (player == 'paper'){
+          if (comp == 'rock') {
+            params.wins++;
+            comment.innerHTML = 'You won!';
+          } else if (comp == 'scissors')  {
+            params.loses++;
+            comment.innerHTML = 'You lost!';
+          } 
+        } else if (player == 'scissors'){
+          if (comp == 'paper') {
+            params.wins++;
+            comment.innerHTML = 'You won!';
+          } else if (comp == 'rock')  {
+            params.loses++;
+            comment.innerHTML = 'You lost!';
+          } 
+        }
+      }
+       
   compare (playerMove, compMove);
-  games = wins + loses;
+  params.games = params.wins + params.loses;
   
   output.innerHTML = 'User: '+playerMove+ '<br> Computer: '+compMove + '<br><br>'; 
-  result.innerHTML = 'You:'+wins+ ' - Computer:'+loses+ '<br><br> Games played: '+games+'.';
+  result.innerHTML = 'You:'+params.wins+ ' - Computer:'+params.loses+ '<br><br> games played: '+params.games+'.';
   gameOver()
 }
+//game over
 function gameOver() {
-  console.log(games)
-  console.log(rounds)
-  if (games == rounds) {
+  console.log(params.games)
+  console.log(params.rounds)
+  if (params.games == params.rounds) {
       document.getElementById("game").style.display = "none";
-      output.innerHTML = 'Game over! <br><br>Finall result: You:'+wins+ ' - Computer:'+loses; 
+      output.innerHTML = 'Game over! <br><br>Finall result: You:'+params.wins+ ' - Computer:'+params.loses; 
       comment.innerHTML = ''
       result.innerHTML = ''
   } 
